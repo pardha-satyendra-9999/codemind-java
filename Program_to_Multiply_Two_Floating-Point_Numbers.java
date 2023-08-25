620d52ea6049ea6b10eb9d86ee48cfbee48ea926
pardha-satyendra-9999 <142394273+pardha-satyendra-9999@users.noreply.github.com>
2023-08-25 11:28:27 +0530

    import java.util.*; public class Par{     public static void main(String[] args)     {         Scanner n=new Scanner(System.in);         float a,b,c;         a=n.nextFloat();         b=n.nextFloat();         c=(a*b);         System.out.printf("%.2f",c);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Par{
+    public static void main(String[] args)
+    {
+        Scanner n=new Scanner(System.in);
+        float a,b,c;
+        a=n.nextFloat();
+        b=n.nextFloat();
+        c=(a*b);
+        System.out.printf("%.2f",c);
+    }
+}
